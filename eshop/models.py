23caa07bae3f0ed915d05e0ec2560da81fdeef8d
root <root@malvina8.fvds.ru>
2019-01-15 15:20:07 +0300

    add same method

---
+++ eshop/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+
+# Create your models here.
+
+class Storage(models.Model):
+    title = models.CharField(max_length=200)
+
+class Pickup(models.Model):
+    title = models.CharField(max_length=200)
+
+class ZoneCourier(models.Model):
+    title = models.CharField(max_length=200)
+
+class Building(models.Model):
+    address = models.CharField(max_length=200)
+
+class Producr(models.Model):
+    text = models.CharField(max_length=200)
+
+class Offer(models.Model):
+    text = models.CharField(max_length=200)
+
+class Order(models.Model):
+    text = models.CharField(max_length=200)
+    offers = []
+
+    def add_offer(self, offer):
+        self.offers.append(offer)
+
+class Shop(models.Model):
+    title = models.CharField(max_length=200)
+    offers = models.ManyToManyField(Offer)
+    pickup_points = models.ManyToManyField(Pickup)
+    storages = models.ManyToManyField(Storage)
+
+    def add_offer_for_user_from_city(self, offer, pickup_in_city):
+        self.offers.add(offer)
+
+    def add_pickup_for_user_from_city(self, pickup_point, pickup_in_city):
+        self.pickup_points.add(pickup_point)
+
+    def add_storage(self, storage):
+        self.storages.add(storage)
+
+    #def list_offer(self, session):
+    def list_offer_for_user_with_pickup_in_city(self, user, pickup_in_city):
+        return sorted(list(self.offers.all()), key=lambda x : x.id)
+
+    def list_pickup(self, session, order):
+        return sorted(list(self.pickup_points.all()), key=lambda x : x.id)
+
+    #def list_storage(self, session, order):
+    def list_storage_for_user_with_pickup_in_city_with_order(self, user, pickup_in_city, order):
+        return sorted(list(self.storages.all()), key=lambda x : x.id)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+class City(models.Model):
+    title = models.CharField(max_length=200)
+
+class Session(models.Model):
+    user = models.ForeignKey(User)
+    city = models.ForeignKey(City)
+
+class CatalogShops(models.Model):
+    title = models.CharField(max_length=200)
+    shops = models.ManyToManyField(Shop)
+
+    #def allow_shops_for_session(self, session):
+    def allow_shops_for_user_with_pickup_in_city(self, user, pickup_in_city):
+        return sorted(list(self.shops.all()), key=lambda x : x.id)
+
+    def add_shop_for_user_from_city(self, shop, user_from_city):
+        self.shops.add(shop)
+
+
+#class Poll(models.Model):
+#    question = models.CharField(max_length=200)
+#    pub_date = models.DateTimeField('date published')
+#
+#class Choice(models.Model):
+#    poll = models.ForeignKey(Poll)
+#    choice = models.CharField(max_length=200)
+#    votes = models.IntegerField()
---
+++ eshop/tests.py
@@ -0,0 +1,146 @@
+#!-*-coding: utf-8 -*-
+from django.test import TestCase
+
+from eshop.models import *
+from django.test.client import Client
+import json
+import sys
+import time
+import pprint
+
+# Create your tests here.
+
+class TestEShop(TestCase):
+    def setUp(self):
+        print "SETUP DATA FOR ..."
+        self.assertEqual.__self__.maxDiff = None
+
+        #catalog_shops = CatalogShops(title=u"main catalog")
+        #catalog_shops.save()
+
+    #def test_list_shop_for_session(self):
+    def test_list_shop_for_user_with_pickup_in_city(self):
+        city_moscow = City(title=u"Moscow")
+        city_spb = City(title=u"SPB")
+
+        user_vova = User(first_name=u"Vova")
+
+        session = Session(user=user_vova, city=city_moscow)
+
+        shop_1 = Shop(title=u"main shop")
+        shop_1.save()
+        shop_2 = Shop(title=u"next shop")
+        shop_2.save()
+
+        catalog_shops = CatalogShops(title=u"main catalog")
+        catalog_shops.save()
+        catalog_shops.add_shop_for_user_from_city(shop_1, city_moscow)
+        catalog_shops.add_shop_for_user_from_city(shop_2, city_spb)
+
+        #shops = catalog_shops.allow_shops_for_session(session)
+        shops = catalog_shops.allow_shops_for_user_with_pickup_in_city(user_vova, city_moscow)
+        self.assertEqual([shop_1, shop_2], shops)
+
+    #def test_list_offer_from_shop_for_session(self):
+    def test_list_offer_from_shop_for_user_with_pickup_in_city(self):
+        city_moscow = City(title=u"Moscow")
+        city_spb = City(title=u"SPB")
+
+        user_vova = User(first_name=u"Vova")
+
+        session = Session(user=user_vova, city=city_moscow)
+
+        offer_1 = Offer(text=u"offer 1 price 1 product 1 quantity 1")
+        offer_1.save()
+        offer_2 = Offer(text=u"offer 2 price 2 product 2 quantity 2")
+        offer_2.save()
+
+        shop = Shop(title=u"main shop")
+        shop.save()
+        shop.add_offer_for_user_from_city(offer_1, city_moscow)
+        shop.add_offer_for_user_from_city(offer_2, city_spb)
+
+        #offers = shop.list_offer(session)
+        offers = shop.list_offer_for_user_with_pickup_in_city(user_vova, city_moscow)
+        self.assertEqual([offer_1, offer_2], offers)
+
+    #def test_list_storage_from_shop_for_session_with_order(self):
+    def test_list_storage_from_shop_for_user_with_pickup_in_city_with_order(self):
+        city_moscow = City(title=u"Moscow")
+        city_spb = City(title=u"SPB")
+
+        user_vova = User(first_name=u"Vova")
+
+        session = Session(user=user_vova, city=city_moscow)
+
+        storage_1 = Storage(title=u"main storage")
+        storage_1.save()
+        storage_2 = Storage(title=u"storage 2")
+        storage_2.save()
+
+        shop = Shop(title=u"main shop")
+        shop.save()
+        shop.add_storage(storage_1)
+
+        offer_1 = Offer(text=u"offer 1 price 1 product 1 quantity 1")
+        offer_2 = Offer(text=u"offer 2 price 2 product 2 quantity 2")
+        order = Order()
+        order.add_offer(offer_1)
+        order.add_offer(offer_2)
+        list_storage = shop.list_storage_for_user_with_pickup_in_city_with_order(user_vova, city_moscow, order)
+        self.assertEqual([storage_1], list_storage)
+
+    #def test_list_pickup_from_shop_for_session_with_order(self):
+    def test_list_pickup_from_shop_for_user_with_pickup_in_city_with_order(self):
+        city_moscow = City(title=u"Moscow")
+        city_spb = City(title=u"SPB")
+
+        user_vova = User(first_name=u"Vova")
+
+        session = Session(user=user_vova, city=city_moscow)
+
+        pickup_1 = Pickup(title=u"pickup 1")
+        pickup_1.save()
+        pickup_2 = Pickup(title=u"pickup 2")
+        pickup_2.save()
+
+        shop = Shop(title=u"main shop")
+        shop.save()
+        shop.add_pickup_for_user_from_city(pickup_1, city_moscow)
+
+        offer_1 = Offer(text=u"offer 1 price 1 product 1 quantity 1")
+        offer_2 = Offer(text=u"offer 2 price 2 product 2 quantity 2")
+        order = Order()
+        order.add_offer(offer_1)
+        order.add_offer(offer_2)
+        list_pickup = shop.list_pickup(session, order)
+        self.assertEqual([pickup_1], list_pickup)
+
+
+
+
+
+
+
+        #customer_record = Entity(title=u"Клиет с улицы")
+        #customer_record.save()
+
+        #executor_record = Entity(title=u"Магазин - И.П.")
+        #executor_record.save()
+
+        #storage_record_1 = Storage(
+        #    title=u"Склад 1",
+        #    owner=executor_record
+        #)
+        #storage_record_1.save()
+
+        #product_record_1 = Product(title=u"pr - 1")
+        #product_record_1.save()
+
+        #offer_record_1 = OfferRecord(
+        #    storage_depart=storage_record_1,
+        #    product=product_record_1,
+        #    quantity=1,
+        #    price=1100,
+        #    datetime_ready_for_deaprt='2018-10-10 20:20:00',
+        #    )
