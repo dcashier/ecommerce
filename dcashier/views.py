9288d6119641397963f3bfc3ac8cca6e15361ad7
root <root@malvina8.fvds.ru>
2019-02-01 20:19:23 +0300

    add login

--- dcashier/views.py
+++ dcashier/views.py
@@ -2,9 +2,57 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
 
+from eactor.models import AuthSystem
+
+from django.template import RequestContext, loader
+
 class MyView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'dcashier/static/index.html')
         #return render(request, 'static/index.html')
         #return HttpResponse('Hello, enother World!')
+
+def index(request):
+
+    print '==='
+    answer = {}
+    if request.session.get('actor_id'):
+        auth_system = AuthSystem()
+        actor = auth_system.get_actor_by_id(request.session.get('actor_id'))
+        answer['actor'] = actor
+        answer['is_login'] = True
+    else:
+        answer['is_login'] = False
+        answer['test'] = 'TESTTT'
+
+    print '--'
+    template = loader.get_template('dcashier/static/index.html')
+    context = RequestContext(request, answer)
+    return HttpResponse(template.render(context))
+
+
+class AuthPage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'dcashier/static/authPage.html')
+
+class Login(View):
+    def post(self, request, *args, **kwargs):
+        phone_number = request.POST['authNameInput']
+        password = request.POST['authPasswordInput']
+
+        auth_system = AuthSystem()
+        if auth_system.has_actor_by_phone_numnber_password(phone_number, password):
+            actor = auth_system.get_actor_by_phone_numnber_password(phone_number, password)
+            request.session['actor_id'] = actor.id
+            return HttpResponse("You're logged in. <a href=\"/\">main</a>")
+	else:
+            return HttpResponse("Your username and password didn't match. <a href=\"/\">main</a>")
+
+class Logout(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            del request.session['actor_id']
+        except KeyError:
+            pass
+        return HttpResponse("You're logged out. <a href=\"/\">main</a>")
