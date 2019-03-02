2fa8e6eed40679d67d0b3b95ef316d3c90288346
22malvina <artemtemp@mail.ru>
2019-03-02 20:15:16 +0300

    fix css static for admin

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,13 +131,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
+##STATIC_URL = '/static/'
+# Artem nead for admoin static
 STATIC_URL = '/css/'
 
+
+# Artem nead for Dcasgier static
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'dcashier/static/css'),
     )
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import sys
 if 'test' in sys.argv:
